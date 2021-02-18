88fc374bd05ce86aea21be54d713abc095de23fc
davidkornel <konel1020@gmail.com>
2021-02-18 14:07:02 +0100

    Add controlplane shell script

---
+++ build/controlplane.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+#
+# Runs Envoy and the example control plane server.  See
+# `internal/example` for the go source.
+#
+
+#Envoy start-up command
+ENVOY=${ENVOY:-/usr/local/bin/envoy}
+
+# Start envoy: important to keep drain time short
+(envoy -c config/ingress-gw.yaml --base-id 1 --drain-time-s 1 -l debug)&
+ENVOY_PID_INGRESS=$!
+(envoy -c config/worker-sidecar.yaml --base-id 2 --drain-time-s 1 -l debug)&
+ENVOY_PID_WORKER=$!
+
+
+function cleanup() {
+  kill ${ENVOY_PID_INGRESS}
+  kill ${ENVOY_PID_WORKER}
+}
+trap cleanup EXIT
+
+#Run the control plane
+bin/controlplane -debug $@
